c94868b03e414102adf807ad0e8a809b125c8b17
rsemkov <136321984+rsemkov@users.noreply.github.com>
2024-03-06 22:32:06 +0200

    JS task

---
+++ 2.2.Arrays_and_Strings-Exercise/8.pascal_splitter.js
@@ -0,0 +1,27 @@
+function pascal_splitter(text_input) {
+    let result_array = []
+    let upper_case_indices = []
+
+    for (let i = 0; i < text_input.length; i++) {
+        current_char = text_input[i]
+        let lower_case_char = current_char.toLowerCase()
+        let upper_case_char = current_char.toUpperCase()
+
+        if (current_char === upper_case_char) {
+            upper_case_indices.push(i)
+        }
+    } 
+
+    for (let a = 0; a < upper_case_indices.length; a++) {
+        let start_idx = upper_case_indices[a]
+        let end_idx = upper_case_indices[a + 1]
+
+        let the_substring = text_input.substring(start_idx, end_idx)
+        result_array.push(the_substring)
+    }
+
+    console.log(result_array.join(', '));
+    
+}
+
+pascal_splitter('SplitMeIfYouCanHaHaYouCantOrYouCan')
