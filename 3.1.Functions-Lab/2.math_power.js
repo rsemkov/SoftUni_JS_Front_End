34c9f7758866463fa6c69498bc1e38b99ed60385
rsemkov <136321984+rsemkov@users.noreply.github.com>
2024-03-17 19:17:19 +0200

    JS functions tasks

---
+++ 3.1.Functions-Lab/2.math_power.js
@@ -0,0 +1,5 @@
+function calcl_math_pow(num, power) {
+    console.log(num ** power);
+}
+
+calcl_math_pow(2, 8)
---
+++ 3.1.Functions-Lab/3.repeat_string.js
@@ -0,0 +1,16 @@
+function rep_str(str, rep_count) {
+    
+    return str.repeat(rep_count)
+}
+
+console.log(rep_str("abc", 3))
+
+// function rep_str(str, rep_count) {
+//     let result = ''
+    
+//     for (let i = 0; i < rep_count; i++) {
+//         result += str
+//     }
+
+//     return result
+// }
