1059dc1134e919616fc702776955f79c9a96a8bb
rsemkov <136321984+rsemkov@users.noreply.github.com>
2024-04-17 16:06:06 +0300

    DOM JS

---
+++ 5.1.DOM_introduction-Lab/02.Collect List Items/CollectListItems.js
@@ -0,0 +1,46 @@
+function extractText() {
+    const items_list = document.getElementById('items')
+    const result = document.getElementById('result')
+    
+    result.textContent = Array(items_list.textContent)
+
+}
+
+
+// function extractText() {
+//     const itemsElement = document.getElementById('items')
+//     const textAreaElement = document.getElementById('result')
+
+//     textAreaElement.value = itemsElement.textContent
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 5.1.DOM_introduction-Lab/03.Sum Numbers/SumNumbers.js
@@ -0,0 +1,10 @@
+
+function calc() {
+    const firstNum = document.getElementById('num1')
+    const secondNum = document.getElementById('num2')
+    const theSum = document.getElementById('sum')
+
+    theSum.value = Number(firstNum.value) + Number(secondNum.value)
+
+}
+
